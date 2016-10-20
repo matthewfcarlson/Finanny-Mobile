e161b79ecd2ff96609f0d0cdea91fecd2544c2de
Matthew Carlson <matthewfcarlson@gmail.com>
2016-10-19 19:39:08 -0600

    Added some test code

--- index.android.js
+++ index.android.js
@@ -17,7 +17,7 @@ class finanny extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native on Android!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
